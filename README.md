65264286f7d649adfa548d3d7826bd00e716d0bf
Igor Iric <iricigor@gmail.com>
2017-07-29 17:45:17 +0200

    all 3 functions coded

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # GitActionsTest2
+
 second testing of merging two branches
+
+## Current status
+
+Step 1 finished
+
+# Details of each step
+
+### Step 1
+
+Create functions TrimFolder, GetFolder and TestFolder
+
+1. **TrimFolder** removes NBSP, SPC and TAB from the end of folder name
+1. **GetFolder** returns folder name unless it finishes with '\\' or '\\*'. In reality this should return folder object, but for now its ok
+1. **TestFolder** checks couple of default behaviours. 'temp\*', 'temp\* ' both should return temp
--- code.ps1
+++ code.ps1
@@ -1,16 +1,32 @@
 function TrimFolder ([string]$FolderName) {
     $TrimmingChars = @([char]160, char[32], char[9])
     $NewFolderName = $FolderName
-    foreach ($Char in $TrimmingChars) {
-        $NewFolderName = $NewFolderName.TrimEnd($Char)
+    $Repeat = $true
+    while ($Repeat) {
+        $Repeat = $false
+        foreach ($Char in $TrimmingChars) {
+            if ($NewFolderName -match ("$Char$")) {
+                $NewFolderName = $NewFolderName.TrimEnd($Char)
+                $Repeat = $true
+            }
+        }
     }
     return $NewFolderName
 }
 
-function GetFolder {
-
+function GetFolder ([string]$FolderName) {
+    if ($FolderName -match '(^.+)(\\*?$)') {
+        return $Matches[1]
+    } else {
+        return $FolderName
+    }
 }
 
 function TestFolder {
-    
+    # test 1
+    (GetFolder 'temp\*') -eq 'temp'
+    # test 2
+    (GetFolder 'temp\ ') -eq 'temp'
+    # test 3
+    (GetFolder 'temp') -eq 'temp'
 }
